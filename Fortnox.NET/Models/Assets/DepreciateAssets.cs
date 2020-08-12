32206ab4f249377910b2d17e7cfa011a025a8ffd
BrokenProgrammer <brokenprogrammer@gmail.com>
2020-08-12 10:51:38 +0200

    Implemented support for performing actions on Assets.

---
+++ Fortnox.NET/Models/Assets/DepreciateAssets.cs
@@ -0,0 +1,14 @@
+﻿
+using FortnoxNET.Utils;
+using System.Collections.Generic;
+
+namespace Fortnox.NET.Models.Assets
+{
+    [JsonPropertyClass("Asset")]
+    public class DepreciateAssets
+    {
+        public string DepreciateUntil { get; set; }
+
+        public List<int> AssetIds { get; set; }
+    }
+}
---
+++ Fortnox.NET/Models/Assets/SellAsset.cs
@@ -0,0 +1,16 @@
+﻿using FortnoxNET.Utils;
+
+namespace Fortnox.NET.Models.Assets
+{
+    [JsonPropertyClass("Asset")]
+    public class SellAsset
+    {
+        public int Percentage { get; set; }
+
+        public int Price { get; set; }
+
+        public string Comment { get; set; }
+
+        public string Date { get; set; }
+    }
+}
