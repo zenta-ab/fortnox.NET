4e63e636c7943805513d5ec6a57e1b1e9b2af89f
BrokenProgrammer <brokenprogrammer@gmail.com>
2020-08-14 12:59:55 +0200

    Added remaining crud support for invoicepayments and bookkeep action.

---
+++ Fortnox.NET/Models/InvoicePayment/CreateOrUpdateInvoicePayment.cs
@@ -0,0 +1,14 @@
+﻿using FortnoxNET.Utils;
+
+namespace Fortnox.NET.Models.InvoicePayment
+{
+    [JsonPropertyClass("InvoicePayment")]
+    public class CreateOrUpdateInvoicePayment
+    {
+        public int Amount { get; set; }
+
+        public int AmountCurrency { get; set; }
+
+        public int InvoiceNumber { get; set; }
+    }
+}
