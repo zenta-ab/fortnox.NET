7ced24ee0662f148abb56bbef78a873d9bc9007d
BrokenProgrammer <brokenprogrammer@gmail.com>
2020-12-02 10:38:53 +0100

    Attempt to pass string to lastmodified.

--- Fortnox.NET.Tests/Fortnox.NET.Tests/OrderTests.cs
+++ Fortnox.NET.Tests/Fortnox.NET.Tests/OrderTests.cs
@@ -94,11 +94,12 @@ namespace FortnoxNET.Tests
         [TestMethod]
         public void SearchOrdersLastModifiedTest()
         {
+            var dateOfThisTest = DateTime.Parse("2019-10-09 15:25:31");
             var halfYearPriorToWhenThisTestWasMade = DateTime.Now.AddDays(-180);
 
             var request = new OrderListRequest(this.connectionSettings.AccessToken, this.connectionSettings.ClientSecret)
             {
-                SearchParameters = new Dictionary<OrderSearchParameters, object> { { OrderSearchParameters.LastModified, halfYearPriorToWhenThisTestWasMade } }
+                SearchParameters = new Dictionary<OrderSearchParameters, object> { { OrderSearchParameters.LastModified, "2019-10-09 15:25:31" } }
             };
 
             var orders = OrderService.GetOrdersAsync(request).GetAwaiter().GetResult();
