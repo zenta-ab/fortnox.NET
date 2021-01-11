58f946384696bdd642155dd5505b92de6b5997d9
BrokenProgrammer <brokenprogrammer@gmail.com>
2021-01-11 11:40:04 +0100

    Added documentation and updated types for Employee models.

--- Fortnox.NET/Models/Employee/EmploymentForm.cs
+++ Fortnox.NET/Models/Employee/EmploymentForm.cs
@@ -1,15 +1,61 @@
+using System.Runtime.Serialization;
+
 namespace FortnoxNET.Models.Employee
 {
     public enum EmploymentForm
     {
+        /// <summary>
+        /// Post with conditional tenure
+        /// </summary>
+        [EnumMember(Value = "TV")]
         TV,
+
+        /// <summary>
+        /// Probationary appointment
+        /// </summary>
+        [EnumMember(Value = "PRO")]
         PRO,
+
+        /// <summary>
+        /// Temporary employment
+        /// </summary>
+        [EnumMember(Value = "TID")]
         TID,
+
+        /// <summary>
+        /// Cover staff
+        /// </summary>
+        [EnumMember(Value = "VIK")]
         VIK,
+
+        /// <summary>
+        /// Project employment
+        /// </summary>
+        [EnumMember(Value = "PRJ")]
         PRJ,
+
+        /// <summary>
+        /// Work experience
+        /// </summary>
+        [EnumMember(Value = "PRA")]
         PRA,
+
+        /// <summary>
+        /// Holiday work
+        /// </summary>
+        [EnumMember(Value = "FER")]
         FER,
+
+        /// <summary>
+        /// Seasonal employment
+        /// </summary>
+        [EnumMember(Value = "SES")]
         SES,
+
+        /// <summary>
+        /// Not employed
+        /// </summary>
+        [EnumMember(Value = "NEJ")]
         NEJ,
     }
 }
--- Fortnox.NET/Models/Employee/PersonelType.cs
+++ Fortnox.NET/Models/Employee/PersonelType.cs
@@ -1,8 +1,19 @@
+using System.Runtime.Serialization;
+
 namespace FortnoxNET.Models.Employee
 {
     public enum PersonelType
     {
+        /// <summary>
+        /// Salaried employee
+        /// </summary>
+        [EnumMember(Value = "TJM")]
         TJM,
+
+        /// <summary>
+        /// Worker
+        /// </summary>
+        [EnumMember(Value = "ARB")]
         ARB,
     }
 }
