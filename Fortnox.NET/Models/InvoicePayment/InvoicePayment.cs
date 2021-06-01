290cc18f535d782cdd269c5b712a7278875d3ade
BrokenProgrammer <brokenprogrammer@gmail.com>
2021-06-01 13:35:47 +0200

    Updated types for InvoicePayment and added documentation.

--- Fortnox.NET/Models/InvoicePayment/InvoicePayment.cs
+++ Fortnox.NET/Models/InvoicePayment/InvoicePayment.cs
@@ -8,30 +8,125 @@ namespace FortnoxNET.Models.InvoicePayment
     [JsonPropertyClass("InvoicePayment")]
     public class InvoicePayment
     {
+        /// <summary>
+        /// Direct URL to the record
+        /// </summary>
         [JsonProperty(PropertyName = "@Url")]
         public string Url { get; set; }
-        public int Amount { get; set; }
-        public bool Booked { get; set; }
+
+        /// <summary>
+        /// Amount of the payment
+        /// </summary>
+        public decimal? Amount { get; set; }
+
+        /// <summary>
+        /// Amount in the specified currency of the payment. Required if Currency is other than SEK
+        /// </summary>
+        public decimal? AmountCurrency { get; set; }
+
+        /// <summary>
+        /// If the payment is booked or not
+        /// </summary>
+        public bool? Booked { get; set; }
+
+        /// <summary>
+        /// Currency of the payment
+        /// </summary>
         public string Currency { get; set; }
-        public int CurrencyRate { get; set; }
-        public int CurrencyUnit { get; set; }
+
+        /// <summary>
+        /// The currency rate
+        /// </summary>
+        public decimal? CurrencyRate { get; set; }
+
+        /// <summary>
+        /// The currency unit
+        /// </summary>
+        public decimal? CurrencyUnit { get; set; }
+
+        /// <summary>
+        /// Invoice number
+        /// </summary>
         public int InvoiceNumber { get; set; }
-        public int Number { get; set; }
-        public DateTime PaymentDate { get; set; }
+
+        /// <summary>
+        /// Payment number
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Date of the payment
+        /// </summary>
+        public DateTime? PaymentDate { get; set; }
+
+        /// <summary>
+        /// Payment source
+        /// </summary>
         public string Source { get; set; }
-        public int AmountCurrency { get; set; }
+
+        /// <summary>
+        /// External invoice reference
+        /// </summary>
         public string ExternalInvoiceReference1 { get; set; }
+
+        /// <summary>
+        /// External invoice reference
+        /// </summary>
         public string ExternalInvoiceReference2 { get; set; }
+
+        /// <summary>
+        /// Customer name of the invoice
+        /// </summary>
         public string InvoiceCustomerName { get; set; }
+
+        /// <summary>
+        /// Customer number of the invoice
+        /// </summary>
         public string InvoiceCustomerNumber { get; set; }
-        public DateTime InvoiceDueDate { get; set; }
+
+        /// <summary>
+        /// Due date of the invoice
+        /// </summary>
+        public DateTime? InvoiceDueDate { get; set; }
+
+        /// <summary>
+        /// OCR of the invoice
+        /// </summary>
         public string InvoiceOCR { get; set; }
+
+        /// <summary>
+        /// Invoice total
+        /// </summary>
         public string InvoiceTotal { get; set; }
+
+        /// <summary>
+        /// Code of the mode of payment
+        /// </summary>
         public string ModeOfPayment { get; set; }
-        public int ModeOfPaymentAccount { get; set; }
+
+        /// <summary>
+        /// Account for the mode of payment
+        /// </summary>
+        public int? ModeOfPaymentAccount { get; set; }
+
+        /// <summary>
+        /// Number of the voucher
+        /// </summary>
         public int? VoucherNumber { get; set; }
+
+        /// <summary>
+        /// Series of the voucher
+        /// </summary>
         public string VoucherSeries { get; set; }
+
+        /// <summary>
+        /// Id of the voucher year
+        /// </summary>
         public int? VoucherYear { get; set; }
+
+        /// <summary>
+        /// WriteOffs of the invoice
+        /// </summary>
         public List<WriteOff> WriteOffs { get; set; }
     }
 }
--- Fortnox.NET/Models/InvoicePayment/InvoicePaymentSubset.cs
+++ Fortnox.NET/Models/InvoicePayment/InvoicePaymentSubset.cs
@@ -8,16 +8,55 @@ namespace FortnoxNET.Models.InvoicePayment
     [JsonPropertyClass("InvoicePayments")]
     public class InvoicePaymentSubset
     {
+        /// <summary>
+        /// Direct URL to the record
+        /// </summary>
         [JsonProperty(PropertyName = "@Url")]
         public string Url { get; set; }
-        public int Amount { get; set; }
-        public bool Booked { get; set; }
+
+        /// <summary>
+        /// Amount of the payment
+        /// </summary>
+        public decimal? Amount { get; set; }
+
+        /// <summary>
+        /// If the payment is booked or not
+        /// </summary>
+        public bool? Booked { get; set; }
+
+        /// <summary>
+        /// Currency of the payment
+        /// </summary>
         public string Currency { get; set; }
-        public int CurrencyRate { get; set; }
-        public int CurrencyUnit { get; set; }
+
+        /// <summary>
+        /// The currency rate
+        /// </summary>
+        public decimal? CurrencyRate { get; set; }
+
+        /// <summary>
+        /// The currency unit
+        /// </summary>
+        public decimal? CurrencyUnit { get; set; }
+
+        /// <summary>
+        /// Invoice number
+        /// </summary>
         public int InvoiceNumber { get; set; }
-        public int Number { get; set; }
-        public DateTime PaymentDate { get; set; }
+
+        /// <summary>
+        /// Payment number
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Date of the payment
+        /// </summary>
+        public DateTime? PaymentDate { get; set; }
+
+        /// <summary>
+        /// Payment source
+        /// </summary>
         public string Source { get; set; }
     }
 }
