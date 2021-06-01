071052e745983901fda6d82409e2ef38fb2f5519
BrokenProgrammer <brokenprogrammer@gmail.com>
2021-06-01 14:43:59 +0200

    Added documentation for Labels

--- Fortnox.NET/Models/Label/Label.cs
+++ Fortnox.NET/Models/Label/Label.cs
@@ -5,8 +5,14 @@ namespace FortnoxNET.Models.Label
     [JsonPropertyClass("Labels")]
     public class Label
     {
+        /// <summary>
+        /// The ID of the label.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Description of the label.
+        /// </summary>
         public string Description { get; set; }
     }
 }
